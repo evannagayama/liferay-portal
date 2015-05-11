9fcd2ee85525c53fc3d18010558d133a7ea8a4e7
Brian Wulbern <brian.wulbern@liferay.com>
2015-05-11 10:22:49 -0700

    LRQA-15587 Remove error ignore for LPS-55154

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1054,15 +1054,6 @@ public class LiferaySeleniumHelper {
 			return true;
 		}
 
-		// LPS-55154, temporary workaround until Shuyang Zhou fixes it
-
-		if (line.contains(
-				"Unable to process message " +
-					"{destinationName=liferay/hot_deploy")) {
-
-			return true;
-		}
-
 		// LRQA-14442, temporary workaround until Kiyoshi Lee fixes it
 
 		if (line.contains("Framework Event Dispatcher: Equinox Container:")) {
