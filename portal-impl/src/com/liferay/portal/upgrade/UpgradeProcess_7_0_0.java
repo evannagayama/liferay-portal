26dff48c3121c272268cdeabb53cab9a4882e9b2
Brian Wulbern <brian.wulbern@liferay.com>
2016-02-04 07:57:02 -0800

    Revert "LPS-61952 Drop off DB support"
    This reverts commit 204e415ab0f54a9b3a8b15df69f633480ca722cf.

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
@@ -20,6 +20,7 @@ import com.liferay.portal.upgrade.v7_0_0.UpgradeAddress;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeAsset;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeAssetTagsResourcePermission;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeBackgroundTask;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeCalEvent;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeCompanyId;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeContact;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeDocumentLibrary;
@@ -69,6 +70,7 @@ public class UpgradeProcess_7_0_0 extends UpgradeProcess {
 		upgrade(UpgradeAsset.class);
 		upgrade(UpgradeAssetTagsResourcePermission.class);
 		upgrade(UpgradeBackgroundTask.class);
+		upgrade(UpgradeCalEvent.class);
 		upgrade(UpgradeCompanyId.class);
 		upgrade(UpgradeContact.class);
 		upgrade(UpgradeDocumentLibrary.class);
